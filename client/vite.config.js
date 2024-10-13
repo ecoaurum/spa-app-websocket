e84e1391e3865de6cfc8fc86741f74cfc50e5d59
Alexander Sakaly <marketdoski@gmail.com>
2024-10-13 05:07:50 +0300

    Updated vite.config.js to use port 80 in production mode, updated client Dockerfile, updated package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,25 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+// 	plugins: [react()],
+// 	host: "0.0.0.0",
+// 	port: 5173,
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	host: "0.0.0.0",
-	port: 5173,
+	server: {
+		host: "0.0.0.0", // Разрешаем доступ извне
+		port: 5173, // Порт для разработки
+	},
+	preview: {
+		port: 80, // Порт для продакшн-сервера
+		host: "0.0.0.0", // Открываем для внешних запросов
+	},
 });
