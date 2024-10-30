00ad9243002de615fd7b99b659b4e8c19bc0c71b
Alexander Sakaly <marketdoski@gmail.com>
2024-10-30 14:20:25 +0200

    changed VITE_API_URL

--- client/src/components/home/Register.jsx
+++ client/src/components/home/Register.jsx
@@ -45,7 +45,7 @@ const Register = () => {
 
 		// Отправляем запрос на сервер для регистрации
 		const response = await fetch(
-			`${import.meta.env.VITE_API_URL}/api/auth/register`,
+			`${import.meta.env.VITE_API_URL}api/auth/register`,
 			{
 				method: "POST",
 				headers: { "Content-Type": "application/json" }, // Заголовки запроса
