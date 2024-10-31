9ac2dfe363d762b489552d663e49edc3cb5b5c44
Alexander Sakaly <marketdoski@gmail.com>
2024-10-31 11:24:15 +0200

    tls: true2

--- server/queues/messageQueue.js
+++ server/queues/messageQueue.js
@@ -12,9 +12,8 @@ const { sanitizeMessage } = require("../middlewares/sanitize"); // Импорт�
 // });
 
 // Подключаем очередь сообщений через REDIS_URL
-const messageQueue = new Queue("messageQueue", {
+const messageQueue = new Queue("messageQueue", process.env.REDIS_URL, {
 	redis: {
-		url: process.env.REDIS_URL,
 		tls: true,
 		enableTLSForSentinelMode: false,
 	},
